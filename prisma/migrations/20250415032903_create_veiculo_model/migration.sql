73d57c8ecce46ec8b43f0bbeaf9edd179afd0a8a
adrhyan16112005 <adrhyanviniciusnunesflores@gmail.com>
2025-07-21 02:15:05 -0300

    VV
    VV

--- prisma/migrations/20250415032903_create_veiculo_model/migration.sql
+++ prisma/migrations/20250415032903_create_veiculo_model/migration.sql
@@ -1,9 +1,8 @@
 -- CreateTable
 CREATE TABLE "Veiculo" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "placa" TEXT NOT NULL,
-    "modelo" TEXT NOT NULL,
-    "cor" TEXT NOT NULL,
-    "ano" INTEGER NOT NULL
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, -- Cria uma chave primária com auto incremento
+    "placa" TEXT NOT NULL,                           -- Campo obrigatório para a placa do veículo
+    "modelo" TEXT NOT NULL,                          -- Campo obrigatório para o modelo do veículo
+    "cor" TEXT NOT NULL,                             -- Campo obrigatório para a cor do veículo
+    "ano" INTEGER NOT NULL                           -- Campo obrigatório para o ano do veículo
 );
-JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ
